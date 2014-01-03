216a5aba6c0e776b0be512dd2c8c5cd4d912b055
Katy Huff <katyhuff@gmail.com>
2014-01-03 11:10:37 -0800

    more customization for gh-pages system with mocdown.

--- doc/gh-project.mk
+++ doc/gh-project.mk
@@ -1,13 +1,13 @@
 # Example for a Github hosted domain account: account.github.io
 #
 # GH_SOURCE_DIR = top-level directory of all the ReST source files
-GH_SOURCE_DIR = source
+GH_SOURCE_DIR = src
 # GH_SOURCE_BRANCH = repository branch that contains the source
-GH_SOURCE_BRANCH = source
+GH_SOURCE_BRANCH = master
 # GH_PUBLISH_BRANCH = repository branch that contains the rendered HTML
-GH_PUBLISH_BRANCH = master
+GH_PUBLISH_BRANCH = gh-pages
 # GH_UPSTREAM_REPO = repository that contains the rendered HTML
-GH_UPSTREAM_REPO = upstream
+GH_UPSTREAM_REPO = origin
 
 
 # Example for a gh-pages project
